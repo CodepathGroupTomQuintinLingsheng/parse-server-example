9936398088e71cea126eda9101167d1c257c66b7
Lingsheng Lyu <lvlingsheng@gmail.com>
2016-04-19 19:13:36 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,31 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define("addfriend", function(request, response) {  
+    var targetuserId = request.params.targetuserId,  
+        currentuserId = request.params.currentuserId;  
+  
+    var query = new Parse.Query("_User");
+    query.equalTo("_id", targetuserId);
+    query.find({
+      success: function(results) {
+
+        var userfollowerId = results[0].get("followerIDs");
+        var targetuser = result[0];
+        if(!userfollowerId){
+          var arr = [];
+          arr.push(currentuserId);
+          targetuser.set("followerIDs", arr);
+        }else{
+          userfollowerId.push(currentuserId);
+          targetuser.set("followerIDs", userfollowerId);
+          //targetuser.push()
+        }
+        response.success("add successfully");
+      },
+      error: function() {
+        response.error("movie lookup failed");
+      }
+  });
+}); 
