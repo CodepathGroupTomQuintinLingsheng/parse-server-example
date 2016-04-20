33f22a9c8307f94e0db2576f746d7a389272d83d
tcliff111 <t.clifford@wustl.edu>
2016-04-20 15:14:53 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -8,6 +8,7 @@ Parse.Cloud.define('addfriend', function(request, response) {
     var targetuserId = request.params.targetuserId,  
         currentuserId = request.params.currentuserId;  
     
+    response.success("hiya");
     // response.success(targetuserId + " " + currentuserId);
     
     // var query = new Parse.Query('Event');
