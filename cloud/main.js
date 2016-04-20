da532bb8f422f0748674bf9b3c75dd0b5f14ae2a
tcliff111 <t.clifford@wustl.edu>
2016-04-20 14:41:39 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -8,32 +8,34 @@ Parse.Cloud.define('addfriend', function(request, response) {
     var targetuserId = request.params.targetuserId,  
         currentuserId = request.params.currentuserId;  
     
-    var query = new Parse.Query('Event');
-    query.equalTo("_id", targetuserId);
-    query.find({
-      success: function(results) {
+    response.success(targetuserId + " " + currentuserId);
+    
+    // var query = new Parse.Query('Event');
+    // query.equalTo("_id", targetuserId);
+    // query.find({
+    //   success: function(results) {
 
-        var userfollowerId = results[0].get('followerIDs');
-        var targetuser = result[0];
-        if(!userfollowerId){
-          var arr = [];
-          arr.push(currentuserId);
-          targetuser.set("followerIDs", arr);
-        }else{
-          userfollowerId.push(currentuserId);
-          targetuser.set("followerIDs", userfollowerId);
-          //targetuser.push()
-        }
+    //     var userfollowerId = results[0].get('followerIDs');
+    //     var targetuser = result[0];
+    //     if(!userfollowerId){
+    //       var arr = [];
+    //       arr.push(currentuserId);
+    //       targetuser.set("followerIDs", arr);
+    //     }else{
+    //       userfollowerId.push(currentuserId);
+    //       targetuser.set("followerIDs", userfollowerId);
+    //       //targetuser.push()
+    //     }
         
-        targetuser.save().then(function(user) {  
-          response.success(user);  
-        }, function(error) {  
-          response.error('error2')  
-        });  
-        response.success("add successfully");
-      },
-      error: function() {
-        response.error("look up failed");
-      }
-  });
+    //     targetuser.save().then(function(user) {  
+    //       response.success(user);  
+    //     }, function(error) {  
+    //       response.error('error2')  
+    //     });  
+    //     response.success("add successfully");
+    //   },
+    //   error: function() {
+    //     response.error("look up failed");
+    //   }
+  // });
 }); 
